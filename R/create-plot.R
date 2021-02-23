a9a90bb104b78e1b48e7eaa81b5f9c1c17401063
sarting118 <giu.miao@hotmail.it>
2021-02-23 16:39:35 +0000

    Faceting on wind direction

--- R/create-plot.R
+++ R/create-plot.R
@@ -17,7 +17,8 @@ df %>%
   ggplot(aes(x = Temp9am, y = Humidity9am, colour = 'red')) + 
   geom_point() +
   ggtitle('Temperature-humidity in Brisbane') +
-  theme_minimal()
+  theme_minimal() +
+  facet_wrap(~WindDir9am)
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
